ee8abbe0d38ca807fe78ba9f380f16c3216f18f4
Cocon <Super@mail.ru>
2019-03-13 00:25:07 +0300

    Добавил иконку ремонта

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace CarLeasingViewer.Properties {
                 return ((System.Drawing.Icon)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon maintenance {
+            get {
+                object obj = ResourceManager.GetObject("maintenance", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
     }
 }
