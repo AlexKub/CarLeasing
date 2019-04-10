7ee8d0e00e2607eae8d3d4e277a7ed496df14c99
Cocon <Super@mail.ru>
2019-04-10 03:51:12 +0300

    Добавил модель для Сторно

---
+++ Models/Storno.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CarLeasingViewer.Models
+{
+    /// <summary>
+    /// Сторнирование
+    /// </summary>
+    public class Storno : IPeriod
+    {
+        public DateTime DateStart { get; set; }
+
+        public DateTime DateEnd { get; set; }
+
+        public DateTime DocumentDate { get; set; }
+
+        public int MonthCount { get;  set; }
+
+        public string Comment { get; set; }
+
+    }
+}
